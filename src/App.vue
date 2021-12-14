7cf260baa7d00ed9cd07f88b44a1c15882feaf16
woow_wu7 <woow.wu7@gmail.com>
2021-12-14 22:23:25 +0800

    feat: layout row col.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <template>
   <div id="app">
-    <el-row tag="div" type="flex" justify="start" align="middle" class="content">
-      <el-col span="12" style="background: red;">111</el-col>
-      <el-col span="6" style="background: green;">222</el-col>
-      <el-col span="6" style="background: gray;">333</el-col>
+    <el-row
+      tag="div"
+      type="flex"
+      justify="start"
+      align="middle"
+      class="content hidden-sm-and-down"
+    >
+      <!-- <el-col :span="12" style="background: red;">111</el-col>
+      <el-col :span="4" :offset="6" :push="2" style="background: green;">222</el-col> -->
+      <!-- <el-col :span="6" style="background: gray;">333</el-col> -->
+
+      <el-col :sm="6" :md="12" style="background: red">1111</el-col>
+      <el-col :sm="6" :md="6" style="background: blue">222</el-col>
+      <el-col :sm="6" :md="3" style="background: grey">333</el-col>
+      <el-col :sm="6" :md="3" style="background: green">444</el-col>
+
     </el-row>
+
+    <!-- 以下是各种测试 -->
+    <TestFor />
   </div>
 </template>
 
 <script>
+import TestFor from "./Test-$placeholder/test-scss-@for/@for.vue";
 export default {
   name: "App",
+  components: {
+    TestFor: TestFor, // 测试 @for $i from x to/through xxx
+  },
 };
 </script>
 
---
+++ src/Test-$placeholder/test-scss/@for.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>1</p>
+    <p>2</p>
+    <p>3</p>
+    <p>4</p>
+    <p>5</p>
+    <p>6</p>
+    <p>7</p>
+    <p>8</p>
+    <p>9</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@for $i from 1 through 4 {
+  // 包含第4个，to不包含4个
+  p:nth-child(#{$i}) {
+    background: red;
+  }
+}
+</style>
