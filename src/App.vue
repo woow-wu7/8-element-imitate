fe488544d81130a393c02352f55acdcd221dcc95
woow_wu7 <woow.wu7@gmail.com>
2021-12-18 00:50:05 +0800

    chore: 8-divine.

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,15 @@
       <!-- <el-col :span="12" style="background: red;">111</el-col>
       <el-col :span="4" :offset="6" :push="2" style="background: green;">222</el-col> -->
       <!-- <el-col :span="6" style="background: gray;">333</el-col> -->
-
       <el-col :sm="6" :md="12" style="background: red">1111</el-col>
       <el-col :sm="6" :md="6" style="background: blue">222</el-col>
       <el-col :sm="6" :md="3" style="background: grey">333</el-col>
       <el-col :sm="6" :md="3" style="background: green">444</el-col>
-
     </el-row>
+
+    <dv-row>
+      <dv-col>7777</dv-col>
+    </dv-row>
 
     <!-- 以下是各种测试 -->
     <TestFor />
@@ -24,7 +26,7 @@
 </template>
 
 <script>
-import TestFor from "./Test-$placeholder/test-scss-@for/@for.vue";
+import TestFor from "./Test-$placeholder/test-scss/@for.vue";
 export default {
   name: "App",
   components: {
--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
 import "./packages/theme-chalk/index.scss";
-import ElementUI from "./index.js";
+// import ElementUI from "./index.js";
 import App from "./App";
+import DivineUi from "8-divine/dist/bundle.js";
 
-Vue.use(ElementUI);
+Vue.use(DivineUi);
+// Vue.use(ElementUI);
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
